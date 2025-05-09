7188a90233fa750e518c284b98dccf833bb570c2
IvanPshenichnyi <153432500+IvanPshenichnyi@users.noreply.github.com>
2025-05-09 10:34:33 +0300

    first  try

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,9 @@
 import { FC } from 'react';
 import { AppHeaderUI } from '@ui';
+import { useSelector } from '../../services/store';
+import { selectCurrentUser } from '../../services/slices/user';
 
-export const AppHeader: FC = () => <AppHeaderUI userName='' />;
+export const AppHeader: FC = () => {
+  const user = useSelector((state) => selectCurrentUser(state.user));
+  return <AppHeaderUI userName={user?.name} />;
+};
